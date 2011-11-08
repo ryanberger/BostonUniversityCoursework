e10c3492695709b1aca9f1b857dc587ae044d1b5
rybooda <rybooda@localhost>
2011-11-07 22:48:36 -0500

    Added input fields for individual calculations.

--- CS683/PowerCostEstimator/src/edu/bu/powercostestimator/CalculateActivity.java
+++ CS683/PowerCostEstimator/src/edu/bu/powercostestimator/CalculateActivity.java
@@ -2,11 +2,23 @@ package edu.bu.powercostestimator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TimePicker;
 
 public class CalculateActivity extends Activity {
+	
 	 public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 
 	        setContentView(R.layout.calculate_layout);
+	        
+	        String[] devices = getResources().getStringArray(R.array.devices_array);
+	        
+	        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewDevice);
+	        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, devices);
+	        textView.setAdapter(adapter);
+	        
+	        
 	    }
 }
