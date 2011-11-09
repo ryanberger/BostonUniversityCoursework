0c934c31a7acbc89b616013797ff5abebeb5fbc3
rberger <rberger@WKW7-2MJJDP1.corp.epnet.com>
2011-11-09 15:56:16 -0500

    Updated calculate layout to be more UI-friendly in landscape mode.

--- CS683/PowerCostEstimator/src/edu/bu/powercostestimator/CalculateActivity.java
+++ CS683/PowerCostEstimator/src/edu/bu/powercostestimator/CalculateActivity.java
@@ -2,23 +2,46 @@ package edu.bu.powercostestimator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.TimePicker;
+import android.widget.EditText;
 
 public class CalculateActivity extends Activity {
+	
+	 private AutoCompleteTextView _deviceField;
+	 private EditText _powerFullField;
 	
 	 public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 
 	        setContentView(R.layout.calculate_layout);
+	        _deviceField = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView_device);
+	        _powerFullField = (EditText) findViewById(R.id.editText_power_full);
 	        
 	        String[] devices = getResources().getStringArray(R.array.devices_array);
 	        
-	        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewDevice);
 	        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, devices);
-	        textView.setAdapter(adapter);
-	        
-	        
+	        _deviceField.setAdapter(adapter);   
 	    }
+	 
+	 /*
+	  * Override default onKeyUp behavior to account for auto-complete field.
+	  * @see android.app.Activity#onKeyUp(int, android.view.KeyEvent)
+	  */
+	 @Override
+	 public boolean onKeyUp(int keyCode, KeyEvent event) {
+		 if (keyCode == KeyEvent.KEYCODE_ENTER) {
+		     if (_deviceField.hasFocus()) {
+			     // sends focus to another field (user pressed "Next")
+		    	 _powerFullField.requestFocus();
+			     return true;
+		     }
+		     else {
+	            // use default behavior
+	    	 	return super.onKeyUp(keyCode, event);
+		     }
+		 }
+		 return false;
+	 }
 }
