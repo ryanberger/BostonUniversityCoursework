74ded29eda18ed5971b577b05ae182293d6edbd4
Ryan Berger <ryanberger@users.noreply.github.com>
2011-11-09 15:56:16 -0500

    Updated calculate layout to be more UI-friendly in landscape mode.

--- CS683/PowerCostEstimator/gen/edu/bu/powercostestimator/R.java
+++ CS683/PowerCostEstimator/gen/edu/bu/powercostestimator/R.java
@@ -18,28 +18,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int autoCompleteTextViewDevice=0x7f060003;
-        public static final int button_submit=0x7f060011;
-        public static final int editTextName=0x7f060013;
-        public static final int editTextPrice=0x7f060015;
-        public static final int editText_power_full=0x7f060006;
-        public static final int editText_power_standby=0x7f06000c;
-        public static final int editText_time_full=0x7f060009;
-        public static final int editText_time_standby=0x7f06000f;
-        public static final int linearLayout1=0x7f060001;
-        public static final int linearLayout2=0x7f060004;
-        public static final int linearLayout3=0x7f060007;
-        public static final int linearLayout4=0x7f06000a;
-        public static final int linearLayout5=0x7f06000d;
-        public static final int linearLayout6=0x7f060010;
-        public static final int tableLayoutProfiles=0x7f060000;
-        public static final int textViewName=0x7f060012;
-        public static final int textViewPrice=0x7f060014;
-        public static final int textView_device=0x7f060002;
-        public static final int textView_power_full=0x7f060005;
-        public static final int textView_power_standby=0x7f06000b;
-        public static final int textView_time_full=0x7f060008;
-        public static final int textView_time_standby=0x7f06000e;
+        public static final int ScrollView01=0x7f060000;
+        public static final int autoCompleteTextView_device=0x7f060001;
+        public static final int button_submit=0x7f060006;
+        public static final int editTextName=0x7f06000a;
+        public static final int editTextPrice=0x7f06000d;
+        public static final int editText_power_full=0x7f060002;
+        public static final int editText_power_standby=0x7f060004;
+        public static final int editText_time_full=0x7f060003;
+        public static final int editText_time_standby=0x7f060005;
+        public static final int linearLayout1=0x7f060008;
+        public static final int linearLayout2=0x7f06000b;
+        public static final int tableLayoutProfiles=0x7f060007;
+        public static final int textViewName=0x7f060009;
+        public static final int textViewPrice=0x7f06000c;
     }
     public static final class layout {
         public static final int calculate_layout=0x7f030000;
