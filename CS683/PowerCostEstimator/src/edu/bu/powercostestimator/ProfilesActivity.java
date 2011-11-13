204370d68a6216563e18cce5a7d08cb3bcf9cfc4
rberger <rberger@WKW7-2MJJDP1.corp.epnet.com>
2011-11-13 15:36:02 -0500

    Further fleshed out calculate layout. Added popup to profiles layout.

--- CS683/PowerCostEstimator/src/edu/bu/powercostestimator/ProfilesActivity.java
+++ CS683/PowerCostEstimator/src/edu/bu/powercostestimator/ProfilesActivity.java
@@ -1,12 +1,73 @@
 package edu.bu.powercostestimator;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class ProfilesActivity extends Activity {
-	 public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
+	
+	private ListView _lv1;
+	private String _lv_arr[]={"Home", "Office", "+ Add New"};
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	
+		setContentView(R.layout.profiles_layout);
+	        
+		_lv1 = (ListView)findViewById(R.id.ListView01);
+		// By using setAdpater method in listview we an add string array in list.
+		_lv1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, _lv_arr));
+		
+		_lv1.setOnItemClickListener(new OnItemClickListener() {
+		    public void onItemClick(AdapterView<?> parent, View view,
+		            int position, long id) {
+		    		showAlert();
+		        }
+		      });
+	}
+	
+	private void showAlert() {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
-	        setContentView(R.layout.profiles_layout);
-	    }
+		alert.setTitle(R.string.label_new_profile);
+		//alert.setMessage("Message");
+
+		// Set an EditText view to get user input
+		LinearLayout layout = new LinearLayout(this);
+		// Set to vertical layout
+		layout.setOrientation(1);
+		final EditText input1 = new EditText(this);
+		final EditText input2 = new EditText(this);
+		input1.setHint(R.string.label_profile_name);
+		input2.setHint(R.string.label_price_per_kwh);
+		layout.addView(input1);
+		layout.addView(input2);
+		alert.setView(layout);
+
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String value = input1.getText().toString();
+				// Do something with value!
+			}
+		});
+
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				// Canceled.
+			}
+		});
+
+		alert.show();
+	}
+	
+
+			            
 }
