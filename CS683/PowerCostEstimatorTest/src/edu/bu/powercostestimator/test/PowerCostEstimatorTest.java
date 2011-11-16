9acae323dec4c77e71ede59500da5cd7f1911cca
rberger <rberger@localhost>
2011-11-15 23:01:31 -0500

    Added initial SQLite tests.

--- CS683/PowerCostEstimatorTest/src/edu/bu/powercostestimator/test/PowerCostEstimatorTest.java
+++ CS683/PowerCostEstimatorTest/src/edu/bu/powercostestimator/test/PowerCostEstimatorTest.java
@@ -1,8 +1,10 @@
 package edu.bu.powercostestimator.test;
 
+import android.database.Cursor;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.TextView;
-import edu.bu.powercostestimator.*;
+import edu.bu.powercostestimator.DatabaseAdapter;
+import edu.bu.powercostestimator.PowerCostEstimator;
 
 public class PowerCostEstimatorTest extends
 		ActivityInstrumentationTestCase2<PowerCostEstimator> {
@@ -10,6 +12,7 @@ public class PowerCostEstimatorTest extends
 	private PowerCostEstimator mActivity;
     private TextView mView;
     private String resourceString;
+    private DatabaseAdapter myDbAdapter;
 
 	public PowerCostEstimatorTest() {
 	      super("edu.bu.powercostestimator", PowerCostEstimator.class);
@@ -19,16 +22,31 @@ public class PowerCostEstimatorTest extends
     protected void setUp() throws Exception {
         super.setUp();
         mActivity = this.getActivity();
-        //mView = (TextView) mActivity.findViewById(edu.bu.powercostestimator.R.id.textview);
-        resourceString = mActivity.getString(edu.bu.powercostestimator.R.string.hello);
+        myDbAdapter = new DatabaseAdapter(mActivity);
+        myDbAdapter.open();
+        mView = (TextView) mActivity.findViewById(edu.bu.powercostestimator.R.id.autoCompleteTextView_device);
+        resourceString = mActivity.getString(edu.bu.powercostestimator.R.string.label_device);
     }
 	
 	public void testPreconditions() {
-	      assertNotNull(mView);
+	      //assertNotNull(mView);
 	}
 	
 	public void testText() {
-	      assertEquals(resourceString,(String)mView.getText());
+	      //assertEquals(resourceString,(String)mView.getText());
 	}
 	
+	public void testCreateDatabase() {
+		myDbAdapter.clearDatabase();
+        myDbAdapter.createDatabase();
+	}
+	
+	public void testSetProfile(){
+		myDbAdapter.setProfile("Home_TEST", 0.10);
+		Cursor c = myDbAdapter.getProfile("Home_TEST");
+		int profileNameColumn = c.getColumnIndex("profile_name");
+		c.moveToFirst();
+        assertEquals(c.getString(profileNameColumn), "Home_TEST");
+        myDbAdapter.deleteProfile("Home_TEST");
+	}
 }
