742ef6320415e611c289d6c673774fac65a9d168
rberger <rberger@localhost>
2011-12-06 22:02:32 -0500

    Cleaned up source code. Added comments.

--- CS683/PowerCostEstimator/src/edu/bu/powercostestimator/DatabaseHelper.java
+++ CS683/PowerCostEstimator/src/edu/bu/powercostestimator/DatabaseHelper.java
@@ -7,36 +7,41 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Database Helper class for initializing SQLite database
  */
-public class DatabaseHelper extends SQLiteOpenHelper{
-	
+public class DatabaseHelper extends SQLiteOpenHelper {
+
 	private static final int DATABASE_VERSION = 1;
-    
+
 	private static String DB_NAME = "PowerCostEstimatorDb";
- 
-    /**
-     * Constructor
-     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
-     * @param context
-     */
-    public DatabaseHelper(Context context) {
-    	super(context, DB_NAME, null, DATABASE_VERSION);
-    }
-    
-    /**
-     * Constructor
-     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
-     * Allows for custom database name and version. Useful for unit testing.
-     * @param context
-     * @param databaseName
-     * @param databaseVersion
-     */
-    public DatabaseHelper(Context context, String databaseName, int databaseVersion) {
-    	super(context, databaseName, null, databaseVersion);
-    }
- 
+
+	/**
+	 * Constructor Takes and keeps a reference of the passed context in order to
+	 * access to the application assets and resources.
+	 * 
+	 * @param context
+	 */
+	public DatabaseHelper(Context context) {
+		super(context, DB_NAME, null, DATABASE_VERSION);
+	}
+
+	/**
+	 * Constructor Takes and keeps a reference of the passed context in order to
+	 * access to the application assets and resources. Allows for custom
+	 * database name and version. Useful for unit testing.
+	 * 
+	 * @param context
+	 * @param databaseName
+	 * @param databaseVersion
+	 */
+	public DatabaseHelper(Context context, String databaseName,
+			int databaseVersion) {
+		super(context, databaseName, null, databaseVersion);
+	}
+
 	@Override
-	public void onCreate(SQLiteDatabase db) { }
- 
+	public void onCreate(SQLiteDatabase db) {
+	}
+
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	}
 }
