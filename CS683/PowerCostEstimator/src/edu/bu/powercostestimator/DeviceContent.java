266e5898ec5b329acb0cb44baab3238877cd21d7
Ryan Berger <ryanberger@users.noreply.github.com>
2011-12-11 18:23:32 -0500

    Finalized export functionality.

--- CS683/PowerCostEstimator/src/edu/bu/powercostestimator/DeviceContent.java
+++ CS683/PowerCostEstimator/src/edu/bu/powercostestimator/DeviceContent.java
@@ -6,13 +6,20 @@ public class DeviceContent {
 	private double normalTime;
 	private double standbyUsage;
 	private double standbyTime;
+	private double dailyCost;
+	private double monthlyCost;
+	private double yearlyCost;
 	
-	public DeviceContent(String deviceName, double normalUsage, double normalTime, double standbyUsage, double standbyTime) {
+	public DeviceContent(String deviceName, double normalUsage, double normalTime, double standbyUsage, double standbyTime,
+			double dailyCost, double monthlyCost, double yearlyCost) {
 		this.deviceName = deviceName;
 		this.normalUsage = normalUsage;
 		this.normalTime = normalTime;
 		this.standbyUsage = standbyUsage;
 		this.standbyTime = standbyTime;
+		this.dailyCost = dailyCost;
+		this.monthlyCost = monthlyCost;
+		this.yearlyCost = yearlyCost;
 	}
 	
 	public String getDeviceName() {
@@ -33,5 +40,17 @@ public class DeviceContent {
 	
 	public double getStandbyTime() {
 		return standbyTime;
+	}
+	
+	public double getDailyCost() {
+		return dailyCost;
+	}
+	
+	public double getMonthlyCost() {
+		return monthlyCost;
+	}
+	
+	public double getYearlyCost() {
+		return yearlyCost;
 	}
 }
